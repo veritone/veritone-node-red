43ca7ad53051158b9672ccb342c30700dfacdcd2
Minh Nguyen <minh.nguyen@setacinq.vn>
2019-02-14 01:22:35 -0800

    support uri from msg

--- node/create-tdo.js
+++ node/create-tdo.js
@@ -1,17 +1,19 @@
 
-const { NewVeritoneAPI, Stringify } = require('../lib/graphql');
+const { NewVeritoneAPI } = require('../lib/graphql');
 const { NewOutput } = require('../lib/output');
 
 function CreateNode(RED, node, config) {
     const api = NewVeritoneAPI(RED.log.debug);
     const {
-        uri, isPublic, name,
+        uri, uriType, isPublic, name,
         startDateTime, updateStopDateTimeFromAsset
     } = config;
+    const getUri = uriType === 'str' ? () => uri : (msg) => msg[uri];
     node.on("input", function (msg) {
         const command = 'createTDOWithAsset';
+        const uri = getUri(msg);
         const input = {
-            uri, isPublic, name, 
+            uri, isPublic, name,
             startDateTime, updateStopDateTimeFromAsset
         };
         const fields = `id,name`;
