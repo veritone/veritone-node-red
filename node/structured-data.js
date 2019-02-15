06734bbb78f094726a7b4fb979e698f61802de8f
Minh Nguyen <minh.nguyen@setacinq.vn>
2019-02-15 03:10:39 -0800

    support dynamic form from data schema

--- node/structured-data.js
+++ node/structured-data.js
@@ -1,15 +1,22 @@
 const { NewVeritoneAPI } = require('../lib/graphql');
 const { NewOutput } = require('../lib/output');
-const { replay } = require('../lib/pipe');
 const mustache = require("mustache");
 
 function CreateNode(RED, node, config) {
     const api = NewVeritoneAPI(RED.log.debug);
-    const { schemaId, dataTemplate, action } = config;
+    const { schemaId, dataProps, action } = config;
+    const makeData = (msg) => {
+        const res = {};
+        if (!dataProps) { return res; }
+        Object.keys(dataProps).forEach(k => {
+            res[k] = mustache.render(dataProps[k], msg.payload);
+        });
+        return res;
+    };
     node.on("input", function (msg) {
         const command = 'createStructuredData';
-        const dataString = mustache.render(dataTemplate, msg);;
-        const data = JSON.parse(dataString);
+        const data = makeData(msg);
+        console.log(data);
         const input = { schemaId, data };
         const fields = `id`;
         const { onError, onResponse } = NewOutput(node, msg);
@@ -19,21 +26,31 @@ function CreateNode(RED, node, config) {
 
 function registerHttpEndpoints(RED) {
     const api = NewVeritoneAPI(RED.log.debug);
-    const getSchema = async () => {
+    const getSchemas = async () => {
         const query = `query y { schemas { records { id, dataRegistry { name } } } }`;
-        const res = await api.Query(query);
-        if (!res.data.data.schemas) {
-            return [];
-        }
-        const { records } = res.data.data.schemas;
+        const { data: { data: { schemas } } } = await api.Query(query);
+        const records = schemas ? schemas.records : null;
         if (!records) { return []; }
         return records
             .map(r => ({ id: r.id, name: r.dataRegistry.name }))
             .sort((a, b) => a.name < b.name ? -1 : 1);
     };
-    const cachedGetSchema = replay(getSchema);
     RED.httpAdmin.get("/veritone/schemas", function (req, res, next) {
-        cachedGetSchema().then(data => res.json(data)).catch(next);
+        getSchemas().then(data => res.json(data)).catch(next);
+    });
+    const getDefinition = async (schemaId) => {
+        const query = `query y { schema(id: "${schemaId}") { definition } }`;
+        const { data: { data: { schema } } } = await api.Query(query);
+        const { definition: { properties } } = schema;
+        const records = Object.keys(properties).map(field => {
+            const { type, title, description, examples } = properties[field];
+            return { field, type, title };
+        });
+        return records.sort((a, b) => a.title < b.title ? -1 : 1);
+    };
+    RED.httpAdmin.get("/veritone/schemas/:id", function (req, res, next) {
+        const { id: schemaId } = req.params;
+        getDefinition(schemaId).then(data => res.json(data)).catch(next);
     });
 }
 
