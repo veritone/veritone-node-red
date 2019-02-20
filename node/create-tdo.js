92f1195da4113874cd286773b9521a2695d4919c
Minh Nguyen <minh.nguyen@setacinq.vn>
2019-02-20 04:43:35 -0800

    emit correct command result

--- node/create-tdo.js
+++ node/create-tdo.js
@@ -2,6 +2,13 @@
 const { NewVeritoneAPI } = require('../lib/graphql');
 const { NewOutput } = require('../lib/output');
 
+async function createTDOWithAsset(api, input) {
+    const command = 'createTDOWithAsset';
+    const fields = `id,name`;
+    const { createTDOWithAsset: res } = api.Mutate(command, input, fields);
+    return res;
+}
+
 function CreateNode(RED, node, config) {
     const api = NewVeritoneAPI(RED.log.debug);
     const {
@@ -11,16 +18,15 @@ function CreateNode(RED, node, config) {
     } = config;
     const getUri = uriType === 'str' ? () => uri : (msg) => msg[uri];
     node.on("input", function (msg) {
-        const command = 'createTDOWithAsset';
         const uri = getUri(msg);
         const input = {
             uri, isPublic, name,
             sourceId, contentType,
             startDateTime, updateStopDateTimeFromAsset
         };
-        const fields = `id,name`;
+
         const { onError, onResponse } = NewOutput(node, msg);
-        api.Mutate(command, input, fields).then(onResponse).catch(onError);
+        createTDOWithAsset(api, input).then(onResponse).catch(onError);
     });
 }
 
