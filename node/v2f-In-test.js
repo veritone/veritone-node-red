5ec7e49782029a0209110e4e8a6a43664efae290
Minh Nguyen <minh.nguyen@setacinq.vn>
2019-03-29 02:40:27 -0700

    support http io

--- node/v2f-In-test.js
+++ node/v2f-In-test.js
@@ -1,10 +1,12 @@
+const bodyParser = require("body-parser");
+
 function createResponseWrapper(node, res) {
     const wrapper = { _res: res };
     const toWrap = [
         "append", "attachment", "cookie", "clearCookie", "download", "end",
         "format", "get", "json", "jsonp", "links", "location",
         "redirect", "render", "send", "sendfile", "sendFile", "sendStatus",
-        "set", "status", "type", "vary"
+        "set", "status", "type", "consty"
     ];
     toWrap.forEach(function (f) {
         wrapper[f] = function () {
@@ -25,12 +27,13 @@ function createResponseWrapper(node, res) {
 function CreateNode(RED, node, config) {
     const { engineMode, mimeType, testPayload } = config;
     node.on("input", function (msg) {
-        const event = JSON.parse(testPayload); // similate event
+        // overwrite payload
+        const event = JSON.parse(testPayload);
         if (event.mimeType === mimeType) {
-            msg.event = msg.payload = event;
+            msg.payload = event;
             node.send([msg, null]);
         } else {
-            msg.event = msg.payload = {
+            msg.payload = {
                 type: 'chunk_processed_status',
                 timestampUTC: Date.now(),
                 taskId: event.taskId,
@@ -43,25 +46,48 @@ function CreateNode(RED, node, config) {
 }
 
 function registerHttpEndpoints(RED) {
-    RED.httpAdmin.post("/veritone/v2f-in-test/:id", RED.auth.needsPermission("inject.write"), function(req,res) {
-        var node = RED.nodes.getNode(req.params.id);
+    const maxApiRequestSize = RED.settings.apiMaxLength || '5mb';
+    const jsonParser = bodyParser.json({ limit: maxApiRequestSize });
+    const errorHandler = function (req, res) {
+        const node = RED.nodes.getNode(req.params.id);
+        if (node != null) {
+            node.warn(err);
+        }
+        res.sendStatus(500);
+    }
+    const handler = function (req, res) {
+        const node = RED.nodes.getNode(req.params.id);
         if (node != null) {
             try {
-                node.receive();
+                const msgid = RED.util.generateId();
+                res._msgid = msgid;
+                const msg = {
+                    _msgid: msgid, req: req,
+                    res: createResponseWrapper(node, res),
+                    payload: req.body
+                };
+                node.receive(msg);
                 res.sendStatus(200);
-            } catch(err) {
+            } catch (err) {
                 res.sendStatus(500);
-                node.error(RED._("inject.failed",{error:err.toString()}));
+                node.error(RED._("inject.v2f-in.failed", { error: err.toString() }));
             }
         } else {
             res.sendStatus(404);
         }
-    });
+    }
+    RED.httpNode.post("/veritone/v2f-in-test/:id", jsonParser, handler, errorHandler);
 }
 
 module.exports = function (RED) {
     const NodeName = 'v2f-in-test';
-    registerHttpEndpoints(RED);
+    if (RED.settings.httpNodeRoot !== false) {
+        try {
+            registerHttpEndpoints(RED);
+        } catch (e) {
+            console.log(e);
+        }
+    }
     RED.nodes.registerType(NodeName, function (config) {
         RED.nodes.createNode(this, config);
         CreateNode(RED, this, config);
