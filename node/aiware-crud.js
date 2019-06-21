443774604c9783b35e85367307aa2dc94a7ce467
Minh Nguyen <minh.nguyen@setacinq.vn>
2019-06-21 11:07:17 -0700

    add watchlist op

--- node/aiware-crud.js
+++ node/aiware-crud.js
@@ -1,73 +1,101 @@
 const { NewVeritoneAPI, GetUserAgent } = require('../lib/graphql');
 const { NewOutput } = require('../lib/output');
-const { get } = require('lodash');
 
 
-async function getWatchlist(api, msg, { schemaId, limit = 0 }, fields) {
-    const query = `query { structuredDataObjects(schemaId: "${schemaId}", limit: ${limit}) { records { id data } } }`;
-    const { structuredDataObjects: res } = await api.Query(query);
-    if (!res || !res.records) { return []; }
-    return res.records.map(r => {
-        const { id, data } = r;
-        const res = { id };
-        Object.keys(data).forEach(k => {
-            if (fields[k]) {
-                res[k] = data[k];
-            }
-        });
-        return res;
-    })
+function makeArgHolder(types, params) {
+    const args = [];
+    const holders = [];
+    Object.keys(params).forEach(p => {
+        const v = params[p];
+        const t = types[p];
+        if (v === undefined || (t !== 'String' && v === '')) {
+            return;
+        }
+        args.push(`$${p}: ${t}`);
+        holders.push(`${p}: $${p}`);
+    });
+    const holderStr = holders.length ? `(${holders.join(', ')})` : '';
+    const argStr = args.length ? `(${args.join(', ')})` : '';
+    return { argStr, holderStr };
 }
 
-async function createWatchlist(api, msg, { schemaId }, props) {
-    const command = 'createWatchlist';
-    const data = renderObj(props, msg);
-    const input = { schemaId, data };
-    const fields = `id schemaId`;
-    const { createWatchlist: res } = await api.Mutate(command, input, fields);
+async function readWatchlist(api, params, fields) {
+    const types = {
+        id: 'ID',
+        maxStopDateTime: 'DateTime',
+        minStopDateTime: 'DateTime',
+        minStartDateTime: 'DateTime',
+        maxStartDateTime: 'DateTime',
+        name: 'String',
+        offset: 'Int = 0',
+        limit: 'Int = 30',
+        orderBy: 'WatchlistOrderBy = createdDateTime',
+        orderDirection: 'OrderDirection = desc',
+        isDisabled: 'Boolean',
+    };
+    const { argStr, holderStr } = makeArgHolder(types, params);
+    const query = `query ${argStr} { 
+        watchlists ${holderStr} { 
+            records { id } 
+        } 
+    }`;
+    const variables = params;
+    const { watchlists: res } = await api.Query(query, variables);
     return res;
 }
 
-async function deleteWatchlist(api, msg, { schemaId, recordId }, props) {
-    const command = 'deleteWatchlist';
-    const id = render(recordId, msg);
-    const input = { schemaId, id };
-    const fields = `id`;
-    const { deleteWatchlist: res } = await api.Mutate(command, input, fields);
+async function createWatchlist(api, params) {
+    const query = `mutation ($input: CreateWatchlist!) { 
+        createWatchlist(input: $input) {
+            id
+        }
+    }`;
+    const input = params;
+    const { createWatchlist: res } = await api.Query(query, { input });
+    return res;
+}
+
+async function deleteWatchlist(api, { id }) {
+    const query = `mutation ($id: ID!) { 
+        deleteWatchlist(id: $id) {
+            id
+        }
+    }`;
+    const { deleteWatchlist: res } = await api.Query(query, { id });
     return res;
 };
 
-async function updateWatchlist(api, msg, { schemaId, recordId }, props) {
-    const command = 'createWatchlist';
-    const id = render(recordId, msg);
-    const data = renderObj(props, msg);
-    const input = { schemaId, id, data };
-    const fields = `id data`;
-    const { createWatchlist: res } = await api.Mutate(command, input, fields);
+async function updateWatchlist(api, { schemaId, recordId }) {
+    const query = `mutation ($input: UpdateWatchlist!) { 
+        updateWatchlist(input: $input) {
+            id
+        }
+    }`;
+    const input = params;
+    const { updateWatchlist: res } = await api.Query(query, { input });
     return res;
 }
 
-const actionWorkers = {
-    watchlist: {
-        create: createWatchlist,
-        query: getWatchlist,
-        delete: deleteWatchlist,
-        update: updateWatchlist,
-    }
+const workers = {
+    'watchlist.create': createWatchlist,
+    'watchlist.update': updateWatchlist,
+    'watchlist.delete': deleteWatchlist,
+    'watchlist.read': readWatchlist,
 };
 
 function CreateNode(RED, node, config) {
-    const { object, action } = config;
-    const { params, props } = actionData[action];
+    const { object, action, actionData } = config;
+    const key = `${object}.${action}`;
+    const { params, props } = actionData[key] || {};
     node.on("input", function (msg) {
-        const api = NewVeritoneAPI(RED.log.debug, GetUserAgent(config), msg);
-        const { onError, onSuccess } = NewOutput(node, msg);
-        const worker = get(actionWorkers, [object, action]);
+        const worker = workers[key];
         if (!worker) {
             node.error(`unknown selected object ${object} action ${action}`);
             return;
         }
-        worker(api, msg, params, props).then(onSuccess).catch(onError);
+        const api = NewVeritoneAPI(RED.log.debug, GetUserAgent(config), msg);
+        const { onError, onSuccess } = NewOutput(node, msg);
+        worker(api, params, props).then(onSuccess).catch(onError);
     });
 }
 
