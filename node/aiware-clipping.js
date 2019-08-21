b0a3245f062a0db9cbf452d68c9915977ab1cc86
Anh Tran <anh.tran@setacinq.vn>
2019-08-21 14:26:04 +0700

    VTN-27382 clipping node

--- node/aiware-clipping.js
+++ node/aiware-clipping.js
@@ -1,18 +1,14 @@
-const { getConfig } = require('../lib/graphql');
+const { NewVeritoneAPI, GetUserAgent, getConfig, UploadFileToTDO } = require('../lib/graphql');
 const { NewOutput } = require('../lib/output');
 const { get } = require('lodash');
 const axios = require("axios");
 const { promisify } = require('util');
-const FormData = require('form-data');
 const fs = require('fs');
 const os = require('os');
 const readFile = promisify(fs.readFile);
 
 const { URL, API_TOKEN, TIMEOUT: timeout } = getConfig(process.env);
-
-const fields = [
-  "tdoId", "startOffsetMs", "endOffsetMs", "engineCategoryId"
-];
+const fields = ["tdoId", "startOffsetMs", "endOffsetMs", "engineCategoryId"];
 
 const fieldValue = (config, field, msg) => {
   const value = config[field];
@@ -20,61 +16,52 @@ const fieldValue = (config, field, msg) => {
   return isStr ? value : get(msg, value);
 };
 
-async function downloadFile(input, node) {
+async function getEngineResults(api, input) {
+  const query = `query {
+    engineResults(tdoId: "${input.tdoId}", engineCategoryIds: ["0481ff76-00c5-4e2d-b73d-2e3aaeef79eb"]){
+      records {
+        jsondata
+        startOffsetMs
+        stopOffsetMs
+        assetId
+        userEdited
+        taskId
+        libraryId
+      }
+    }
+  }`;
+  const { engineResults } = await api.Query(query);
+  return engineResults;
+};
+
+async function downloadFile(input) {
   const download_url = `${URL.replace('/v3/graphql','')}/media-streamer/download/tdo/${input.tdoId}?startOffsetMs=${input.startOffsetMs}&endOffsetMs=${input.endOffsetMs}`;
   const headers = { "Authorization": `Bearer ${API_TOKEN}`,"Content-Type": "application/json" };
-  try {
-    return axios({ method: "GET", url: download_url, responseType: 'arraybuffer', headers, timeout});
-  } catch (error) {
-    node.log(error);
-  }
+  return axios({ method: "GET", url: download_url, responseType: 'arraybuffer', headers, timeout});
 }
 
-async function createAsset(input, node) {
-  const formData = new FormData();
-  const file = await downloadFile(input, node);
-  const headers = { 
-    Authorization:  `Bearer ${API_TOKEN}`,
-    ...formData.getHeaders()
-  };
+async function createAsset(api, input, node) {
+  const file = await downloadFile(input);
+  const engineResults = await getEngineResults(api, input);
+  node.log(engineResults);
   const time = (new Date()).getTime();
   const ext = file.headers['content-disposition'].split('="').pop().split('"')[0].split('.').pop();
   const fileName = time + "." + ext;
-  await fs.writeFileSync(os.tmpdir() + '/' + fileName, file);
-  const query =  `mutation {
-    createAsset(input: {
-      containerId: "${input.tdoId}"
-      contentType: "${file.headers['content-type']}"
-      details: "{ 'engineCategoryId': ${input.engineCategoryId} }"
-      assetType: "media"
-    }) {
-      id
-      uri
-      signedUri
-    }
-  }`;
-  
+  fs.writeFileSync(os.tmpdir() + '/' + fileName, file);
   const buffer = await readFile(os.tmpdir() + '/' + fileName);
-  formData.append('query', query);
-  formData.append('file', buffer, { filename: fileName }); 
-  const r = await axios({
-    method: 'POST',
-    url: URL,
-    data: formData,
-    headers
-  });
-  return r.data;
+  node.log(JSON.stringify(input));
+  const r = UploadFileToTDO(input.tdoId, buffer, file.headers['content-type']);
+  return r;
 }
 
 function CreateNode(RED, node, config) {
   node.on("input", function (msg) {
+    const api = NewVeritoneAPI(RED.log.debug, GetUserAgent(config), msg);
     const input = {};
-    fields.forEach(field => {
-        input[field] = fieldValue(config, field, msg);
-    });
+    fields.forEach(field => input[field] = fieldValue(config, field, msg));
     const { onError, onSuccess } = NewOutput(node, msg);
     node.status({ fill: "blue", shape: "dot", text: "processing...   " });
-    createAsset(input, node).then(onSuccess).catch(onError);
+    createAsset(api, input, node).then(onSuccess).catch(onError);
   });
 }
 
