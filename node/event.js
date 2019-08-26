cea317b5b98d92159d2116e0fa21e3c83985ade2
seta-minhnguyen <mnguyen2@veritone.com>
2019-08-26 15:23:12 +0000

    rework on the event handler

--- node/event.js
+++ node/event.js
@@ -1,199 +1,90 @@
+const bodyParser = require("body-parser");
+const { NewOutput } = require('../lib/output');
+const { NewVeritoneAPI, GetUserAgent } = require('../lib/graphql');
+const { NodeRedEnv } = require('../lib/env');
+
+function RemoveNodeRedRoute(RED, url, method = 'post') {
+  const routes = RED.httpNode._router.stack;
+  for (let i = 0; i < routes.length; i++) {
+    const { route } = routes[i];
+    if (route && route.path === url && route.methods[method]) {
+      routes.splice(i, 1);
+      RED.log.debug(`removed route [${method} ${url}]`);
+      return;
+    }
+  }
+}
+
+async function Subscribe(api, { event_name, event_type, app, webhook_url }) {
+  const query = `mutation {
+    subscribeEvent(input: {
+      eventName: "${event_name}"
+      eventType: "${event_type}"
+      application: "${app}"
+      delivery:{
+        name:Webhook
+        params:{ url:"${webhook_url}", encoding:"json" }
+      }
+    })
+  }`;
+  const { subscribeEvent } = await api.Query(query);
+  return subscribeEvent;
+};
+
+async function Unsubscribe(api, subscriptionId) {
+  const query = `mutation{
+    unsubscribeEvent(id:"${subscriptionId}") { id } }
+  `;
+  await api.Query(query);
+}
+
+function CreateNode(RED, node, config) {
+  if (!RED.settings.httpNodeRoot) {
+    node.status({ fill: 'red', shape: 'ring', text: 'http node root disabled' });
+    return;
+  }
+
+  const api = NewVeritoneAPI(RED.log.debug, GetUserAgent(config));
+  const maxApiRequestSize = RED.settings.apiMaxLength || '5mb';
+  const jsonParser = bodyParser.json({ limit: maxApiRequestSize });
+  const errorHandler = (err, req, res) => { node.warn(err); res.sendStatus(500); };
+  const handler = (req, res) => {
+    node.send({
+      _msgid: RED.util.generateId(),
+      payload: req.body
+    });
+    res.sendStatus(200);
+  }
+  const { onSuccess, onError } = NewOutput(node);
+  const { event_type, event_name, app } = config;
+  const uri = `/${config.id.replace(".", "-")}`;
+  const webhook_url = NodeRedEnv.GetAbsUrl(uri);
+  Subscribe(api, { event_name, event_type, app, webhook_url })
+    .then(subscriptionId => {
+      onSuccess(subscriptionId, 'aiware.connected');
+      RED.log.debug(`subscribe ${subscriptionId} created to ${webhook_url}`);
+      RED.httpNode.post(uri, jsonParser, handler, errorHandler);
+      // the flow has been stopped or the node has been deleted
+      // unsubscribe from the event and remove the http handler
+      node.on('close', () => {
+        RemoveNodeRedRoute(RED, uri);
+        Unsubscribe(api, subscriptionId).then(() => {
+          RED.log.debug(`unsubscribe ${subscriptionId} succeeded`);
+        }).catch(e => {
+          RED.log.error(`unsubscribe ${subscriptionId} error`, e);
+        });
+      });
+    }, e => onError(e, 'disconnected'))
+    .catch(e => {
+      const message = 'error ' + e.message;
+      node.status({ fill: 'red', shape: 'ring', text: message });
+    });
+};
+
 module.exports = function (RED) {
-  const bodyParser = require("body-parser");
-
-  function rawBodyParser(req, res, next) {
-    if (req.skipRawBodyParser) {
-      next();
-    } // don't parse this if told to skip
-    if (req._body) {
-      return next();
-    }
-    req.body = "";
-    req._body = true;
-
-    var isText = true;
-    var checkUTF = false;
-
-    if (req.headers["content-type"]) {
-      var parsedType = typer.parse(req.headers["content-type"]);
-      if (parsedType.type === "text") {
-        isText = true;
-      } else if (parsedType.subtype === "xml" || parsedType.suffix === "xml") {
-        isText = true;
-      } else if (parsedType.type !== "application") {
-        isText = false;
-      } else if (parsedType.subtype !== "octet-stream") {
-        checkUTF = true;
-      } else {
-        // applicatino/octet-stream
-        isText = false;
-      }
-    }
-
-    getBody(
-      req, {
-        length: req.headers["content-length"],
-        encoding: isText ? "utf8" : null
-      },
-      function (err, buf) {
-        if (err) {
-          return next(err);
-        }
-        if (!isText && checkUTF && isUtf8(buf)) {
-          buf = buf.toString();
-        }
-        req.body = buf;
-        next();
-      }
-    );
-  }
-
-  function createResponseWrapper(node, res) {
-    var wrapper = {
-      _res: res
-    };
-    var toWrap = [
-      "append",
-      "attachment",
-      "cookie",
-      "clearCookie",
-      "download",
-      "end",
-      "format",
-      "get",
-      "json",
-      "jsonp",
-      "links",
-      "location",
-      "redirect",
-      "render",
-      "send",
-      "sendfile",
-      "sendFile",
-      "sendStatus",
-      "set",
-      "status",
-      "type",
-      "vary"
-    ];
-    toWrap.forEach(function (f) {
-      wrapper[f] = function () {
-        node.warn(
-          RED._("httpin.errors.deprecated-call", {
-            method: "msg.res." + f
-          })
-        );
-        var result = res[f].apply(res, arguments);
-        if (result === res) {
-          return wrapper;
-        } else {
-          return result;
-        }
-      };
-    });
-    return wrapper;
-  }
-
-  function SubscribeEvent(config) {
+  const NodeName = 'event in';
+  RED.nodes.registerType(NodeName, function (config) {
     RED.nodes.createNode(this, config);
-    var node = this;
-    if (!process.env.VERITONE_API_BASE_URL) {
-      throw new Error("VERITONE_API_BASE_URL env variable not set");
-    }
-    if (!process.env.NODE_INSTANCE_URL) {
-      throw new Error("NODE_INSTANCE_URL env variable not set");
-    }
-    this.veritoneUrl =
-      "https://" + process.env.VERITONE_API_BASE_URL + "/v3/graphql";
-    this.url = "/" + config.id.replace(".", "-");
-    this.nodeUrl = "https://" + process.env.NODE_INSTANCE_URL + this.url;
-    if (process.env.AUTH_TOKEN) {
-      this.nodeUrl = this.nodeUrl + "?authToken=" + process.env.AUTH_TOKEN;
-    }
-    this.token = process.env.API_TOKEN;
-    this.app = config.app;
-    this.event = config.event_name;
-    this.type = config.event_type;
-    const apiUtil = require("../utils/api-util.js")(
-      this.veritoneUrl,
-      this.token
-    );
-
-    this.errorHandler = function (err, req, res, next) {
-      node.warn(err);
-      res.sendStatus(500);
-    };
-
-    this.callback = function (req, res) {
-      var msgid = RED.util.generateId();
-      res._msgid = msgid;
-      node.send({
-        _msgid: msgid,
-        req: req,
-        res: createResponseWrapper(node, res),
-        payload: req.body
-      });
-      return res.status(200).jsonp(req.body);
-    };
-
-    apiUtil
-      .subscribeEvents(this.event_name, this.event_type, this.app, this.nodeUrl)
-      .then(subscriptionId => {
-        this.subscriptionId = subscriptionId;
-        RED.httpNode.post(
-          this.url,
-          jsonParser,
-          urlencParser,
-          rawBodyParser,
-          this.callback,
-          this.errorHandler
-        );
-        this.status({
-          fill: "green",
-          shape: "dot",
-          text: "connected"
-        });
-      })
-      .catch(err => {
-        this.error(err);
-        this.status({
-          fill: "red",
-          shape: "ring",
-          text: "disconnected"
-        });
-      });
-
-    var maxApiRequestSize = RED.settings.apiMaxLength || "5mb";
-    var jsonParser = bodyParser.json({
-      limit: maxApiRequestSize
-    });
-    var urlencParser = bodyParser.urlencoded({
-      limit: maxApiRequestSize,
-      extended: true
-    });
-
-    RED.httpNode.post(
-      this.url,
-      jsonParser,
-      urlencParser,
-      rawBodyParser,
-      this.callback,
-      this.errorHandler
-    );
-
-    this.on("close", function () {
-      var node = this;
-      apiUtil.unsubscribeEvents(this.subscriptionId);
-      RED.httpNode._router.stack.forEach(function (route, i, routes) {
-        if (
-          route.route &&
-          route.route.path === node.url &&
-          route.route.methods["post"]
-        ) {
-          routes.splice(i, 1);
-        }
-      });
-    });
-  }
-  RED.nodes.registerType("event in", SubscribeEvent);
+    CreateNode(RED, this, config);
+  });
 };
